5bbda2cc5b410e1d58eb6a1339fd5165d8d77f9c
Poudima Y. Hodalo <56408166+yaohodalo@users.noreply.github.com>
2020-10-16 00:13:40 -0400

    Update countries.js

--- Lab_5/countries.js
+++ Lab_5/countries.js
@@ -300,7 +300,7 @@ listCountries.forEach(element => {
       console.log('Country: ${element.name}, code: ${element.code}'); 
     }
   });
-  function getRandomInclusive(min, max){
+  function getRandomIntInclusive(min, max){
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
